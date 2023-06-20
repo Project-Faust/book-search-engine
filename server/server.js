ec258c8dab915a9f001a14bbad529309cbd3ebc6
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-20 18:55:12 -0400

    imported dependencies and added boilerplate server code

--- server/server.js
+++ server/server.js
@@ -3,11 +3,33 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// import apollo server and schema dependencies
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// declare and assign apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//create instance of apollo server w/GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -19,3 +41,6 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+// call async func to start apollo server
+startApolloServer(typeDefs, resolvers);
