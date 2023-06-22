fa271c690461c36f4f304a3bfcbc8f15a3a05d36
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-22 18:33:48 -0400

    added queries to find user (by id or username) and current user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,29 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+        user: async (_, { userID }) => {
+            const foundUser = await User.findOne({
+                $or: [{ _id: userID }, { username: userID }],
+            });
+
+            if (!foundUser) {
+                throw new Error('Cannot find a user with this ID or username!');
+            }
+
+            return foundUser;
+        },
+        me: async (_, __, { currentUser }) => {
+            if (!currentUser) {
+                throw new Error('You are not logged in.');
+            }
+
+            return currentUser;
+        },
+    }
+}
