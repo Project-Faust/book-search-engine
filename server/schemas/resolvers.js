f1b61ead70c1f3929b9e8e0b5f77c098f78ae650
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-22 18:34:10 -0400

    added queries to find user (by id or username) and current user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,9 +4,6 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        users: async () => {
-            return User.find();
-        },
         user: async (_, { userID }) => {
             const foundUser = await User.findOne({
                 $or: [{ _id: userID }, { username: userID }],
