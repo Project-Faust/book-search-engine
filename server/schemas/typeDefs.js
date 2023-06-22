0a48e6b4ffa0fa5149d5a53b956e881b5fd6af5d
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-22 18:33:11 -0400

    created user, auth, query, and mutations for User model; added basic functionality

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,24 @@
 const { gql } = require('apollo-server-express');
+const { getSingleUser } = require('../controllers/user-controller');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        password: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        user(userID: ID!): User
+        me: User
+    }
+    type Mutation {
+        addUser(name: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
+`
+module.exports = typeDefs;
