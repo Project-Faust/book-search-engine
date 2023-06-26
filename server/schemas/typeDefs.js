4171903418b9623c7cd778194ea3f25ac314f542
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-26 18:36:49 -0400

    updated all base functionality for mutations and types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,18 +6,28 @@ const typeDefs = gql`
         username: String
         email: String
         password: String
+        savedBooks: [Book]
+    }
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
     }
     type Auth {
         token: ID!
         user: User
     }
     type Query {
-        # gets single user by id or username
         getSingleUser(userID: ID!): User
     }
     type Mutation {
         createUser(name: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
+        saveBook(book: String!, user: String!): User
+        deleteBook(bookId: String!, user: String!): User
     }
 `
 module.exports = typeDefs;
