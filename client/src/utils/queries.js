3f55e119ad7eeac3975ecd460a7dc1b5a15208a4
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-26 21:51:19 -0400

    updated query params

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -18,3 +18,22 @@ query getSingleUser($username: String!) {
         }
     }
 `;
+
+export const QUERY_ME = gql`
+query me {
+    me {
+        _id
+        username
+        email
+        savedBooks {
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}
+`;
