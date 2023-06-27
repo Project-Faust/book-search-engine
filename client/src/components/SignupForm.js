9dbbdf60f1cd20aaccd5dbe92323027c60bbee42
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-26 21:54:32 -0400

    replaced API fetch with gql query/mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
@@ -15,7 +15,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
